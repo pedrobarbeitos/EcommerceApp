d5fe7b040158254392770185df898f3c05cafffc
Pedro Barbeitos <barbeitos.pedro@gmail.com>
2023-07-29 10:37:16 +0100

    Navbar Styling

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -8,15 +8,16 @@ import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined
 import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
 import ShoppingCartRoundedIcon from '@mui/icons-material/ShoppingCartRounded';
 
-
+//Style Imports
+import "./Navbar.scss"
 
 const Navbar = () => {
   return (
     <div className="navbar">
         <div className="wrapper">
-         <div className="left"></div>
+         <div className="left">
           <div className="item">
-            <img src="images/en.png" alt=""/>
+            <img src="/images/en.png" alt=""/>
             <KeyboardArrowDownIcon />
           </div>
           <div className="item">
@@ -26,11 +27,43 @@ const Navbar = () => {
           <div className="item">
             <Link to="/products/women">Women</Link>
           </div>
-         <div className="center"></div>
-         <div className="right"></div>
+          <div className="item">
+            <Link to="/products/men">Men</Link>
+          </div>
+          <div className="item">
+            <Link to="/products/children">Children</Link>
+          </div>
+          </div>
+         <div className="center">
+          <Link to="/">PEDROSTORE</Link>
+         </div>
+         <div className="right">
+         <div className="item">
+          <Link to="/">Homepage</Link>
+         </div>
+         <div className="item">
+          <Link to="/">About</Link>
+         </div>
+         <div className="item">
+          <Link to="/">Contact</Link>
+         </div>
+         <div className="item">
+          <Link to="/">Stores</Link>
+         </div>
+         <div className="icons">
+          <SearchIcon/>
+          <PersonOutlineOutlinedIcon/>
+          <FavoriteBorderOutlinedIcon/>
+          <div className="cartIcon">
+            <ShoppingCartRoundedIcon/>
+            <span>0</span>
+          </div>
+
+         </div>
+         </div>
         </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
