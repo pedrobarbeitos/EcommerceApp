0ce05768d2ba9996463e1e469b793aef580571c2
Pedro Barbeitos <barbeitos.pedro@gmail.com>
2023-07-29 15:09:03 +0100

    Footer Basic Design Complete

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,8 +1,49 @@
-import React from 'react'
+import React from 'react';
+import "./Footer.scss";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="footer">
+      <div className="top">
+        <div className="item">
+          <h1>Categories</h1>
+          <span>Women</span>
+          <span>Men</span>
+          <span>Shoes</span>
+          <span>Acessories</span>
+          <span>New Arrivals</span>
+        </div>
+        <div className="item">
+        <h1>Links</h1>
+          <span>FAQ</span>
+          <span>Pages</span>
+          <span>Stores</span>
+          <span>Compare</span>
+          <span>Cookies</span>
+        </div>
+        <div className="item">
+        <h1>About</h1>
+          <span>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          </span>
+        </div>
+        <div className="item">
+        <h1>Contact</h1>
+          <span>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          </span>
+        </div>
+      </div>
+      <div className="bottom">
+        <div className="left">
+          <span className="logo">Pedrostore</span>
+          <span className="copyright">© copyright 2023. All rights reserved</span>
+        </div>
+        <div className="right">
+          <img src="/images/payment.png" alt="" />
+        </div>
+      </div>
+    </div>
   )
 }
 
