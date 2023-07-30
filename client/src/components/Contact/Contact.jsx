346fd3adb595e3f19e87b20e8e8fbae1e7785b5d
Pedro Barbeitos <barbeitos.pedro@gmail.com>
2023-07-30 21:45:44 +0100

    Categories Search Component Created

--- client/src/components/Contact/Contact.jsx
+++ client/src/components/Contact/Contact.jsx
@@ -1,31 +1,30 @@
-import React from 'react';
-
+import React from "react";
+import "./Contact.scss";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import TwitterIcon from "@mui/icons-material/Twitter";
 import GoogleIcon from "@mui/icons-material/Google";
 import PinterestIcon from "@mui/icons-material/Pinterest";
-import "./Contact.scss";
 
-function Contact() {
+const Contact = () => {
   return (
     <div className="contact">
-        <div className="wrappper">
-            <span>Contact us</span>
-            <div className="mail">
-                <input type="text" placeholder="Enter your email..." />
-                <button>JOIN US</button>
-            </div>
-            <div className="icons">
-            <FacebookIcon />
-            <InstagramIcon />
-            <TwitterIcon />
-            <GoogleIcon />
-            <PinterestIcon />
-            </div>
+      <div className="wrapper">
+        <span>CONTACT US:</span>
+        <div className="mail">
+          <input type="text" placeholder="Enter your e-mail..." />
+          <button>JOIN US</button>
         </div>
+        <div className="icons">
+          <FacebookIcon />
+          <InstagramIcon />
+          <TwitterIcon />
+          <GoogleIcon />
+          <PinterestIcon />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
--- client/src/pages/Products/Products.jsx
+++ client/src/pages/Products/Products.jsx
@@ -1,9 +1,57 @@
 import React from 'react';
+import { useState } from "react";
+import { useParams } from "react-router-dom";
+import List from '../../components/List/List';
 import "./Products.scss";
 
 const Products = () => {
+
+  const catId = parseInt(useParams().id);
+  const [maxPrice,setMaxPrice] = useState(1000);
+  const [sort,setSort] = useState(null);
+
   return (
-    <div className="products">Products</div>
+    <div className="products">
+      <div className="left">
+        <div className="filterItem">
+          <h2>Product Categories</h2>
+          <div className="inputItem">
+            <input type="checkbox" id="1" vlaue={1}/>
+            <label htmlFor="1">Shoes</label>
+          </div>
+          <div className="inputItem">
+            <input type="checkbox" id="2" vlaue={2}/>
+            <label htmlFor="2">Skirts</label>
+          </div>
+          <div className="inputItem">
+            <input type="checkbox" id="3" vlaue={3}/>
+            <label htmlFor="3">Coats</label>
+          </div>
+        </div>
+        <div className="filterItem">
+          <h2>Filter by Price</h2>
+          <div className="inputItem">
+            <span>0</span>
+            <input type="range" min={0} max={1000} onChange={(e)=>setMaxPrice(e.target.value)}/>
+            <span>{maxPrice}</span>
+          </div>
+        </div>
+        <div className="filterItem">
+          <h2>Sort by</h2>
+          <div className="inputItem">
+            <input type="radio" id="asc" value="asc" name="price" onChange={e=>setSort("asc")} />
+            <label htmlFor="asc">Price (Lowest First)</label>
+          </div>
+          <div className="inputItem">
+            <input type="radio" id="asc" value="asc" name="price" onChange={e=>setSort("desc")} />
+            <label htmlFor="asc">Price (Highest First)</label>
+          </div>
+        </div>
+      </div>
+      <div className="right">
+        <List catId={catId} maxPrice={maxPrice} sort={sort}/>
+      </div>
+    </div>
   )
 }
 
