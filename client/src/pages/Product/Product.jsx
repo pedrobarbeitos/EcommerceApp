f36a92efa0634dcb6b340cc1afe58287c8f0e74d
Pedro Barbeitos <barbeitos.pedro@gmail.com>
2023-07-31 21:59:42 +0100

    Product page created

--- client/src/pages/Product/Product.jsx
+++ client/src/pages/Product/Product.jsx
@@ -1,9 +1,57 @@
 import React from 'react';
+import {useState} from "react";
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import BalanceIcon from '@mui/icons-material/Balance';
+
+
 import "./Product.scss";
 
+const images = [
+  "https://images.pexels.com/photos/818992/pexels-photo-818992.jpeg?auto=compress&cs=tinysrgb&w=1600",
+  "https://images.pexels.com/photos/2036646/pexels-photo-2036646.jpeg?auto=compress&cs=tinysrgb&w=1600"
+];
+
 const Product = () => {
+
+ const [selectedImg,setSelectedImg] = useState(0);
+ const [quantity,setQuantity] = useState(1);
+
   return (
-    <div className="product">Product</div>
+    <div className="product">
+      <div className="left">
+        <div className="images">
+          <img src={images[0]} alt="" onClick={e=>setSelectedImg(0)} />
+          <img src={images[1]} alt="" onClick={e=>setSelectedImg(1)}/>
+        </div>
+        <div className="mainImg">
+          <img src={images[selectedImg]} alt="" />
+        </div>
+      </div>
+      <div className="right">
+        <h1>Title</h1>
+        <span className="price">£198</span>
+        <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        </p>
+        <div className="quantity">
+          <button onClick={()=>setQuantity(prev=>prev === 1 ? 1 : prev-1 )}>-</button>
+          {quantity}
+          <button onClick={()=>setQuantity(prev=>prev+1)}>+</button>
+        </div>
+        <button className="add">
+          <AddShoppingCartIcon/>ADD TO CART
+        </button>
+        <div className="links">
+          <div className="item">
+            <FavoriteBorderIcon/> ADD TO WISHLIST
+          </div>
+          <div className="item">
+            <BalanceIcon/>ADD TO COMPARE
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
