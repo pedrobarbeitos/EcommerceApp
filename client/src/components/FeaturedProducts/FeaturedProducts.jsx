3869fb836b3460dd29e7f962d8318dfe5fc14d4b
Pedro Barbeitos <barbeitos.pedro@gmail.com>
2023-08-05 11:35:36 +0100

    API access configured (sort of)
    Can't reference process.env.
    Had to type the url and token directly.

--- client/src/components/FeaturedProducts/FeaturedProducts.jsx
+++ client/src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import {useState, useEffect} from "react"
+import axios from "axios"
 import "./FeaturedProducts.scss"
 import Card from '../../components/Card/Card';
 
@@ -40,6 +42,30 @@ function FeaturedProducts({type}) {
     },
   ]
 
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+        try {
+            const data = await axios.get(
+                "http://localhost:1337/api/products",
+                {
+                    headers:{
+                        Authorization: "Bearer d5ebe562187669b23fffb7af54713eb35bf3dc68dd63bb569107f2da9ff392d4ce4852fb2a3d05b74b2e03f43cf37e75975616079afd4775deb3fff7ee3436535052c3ab4f874add8b5a930f21098b55289369cd752071c7c92e5bff689b6c06d84d0d801993c02072ba208f7a9301375a58b4464c3e4d5f39f379dca2cc0873",
+                    },
+                }
+                );
+            console.log(data);
+                    } catch (err) {
+            console.log(err);
+        }
+    };
+    fetchData();
+  }, []);
+
+
+
+
   return (
     <div className="featuredProducts">
         <div className="top">
@@ -58,4 +84,7 @@ function FeaturedProducts({type}) {
 }
 
 export default FeaturedProducts
+
+
+
 
